e2aa8dcee2eeefcc295e5c7aad8533e9bdfb0f09
Spares <macdonald.glen.a@gmail.com>
2024-06-24 23:01:48 +1000

    set digital ocean postgres DB

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{configuration::get_configuration, startup::run};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use std::net::TcpListener;
-use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -11,10 +10,9 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     // connect_lazy only tries to establish a connection when the pool is used for the first time
-    let connection_pool = PgPool::connect_lazy(
-            &configuration.database.connection_string().expose_secret()
-        )
-        .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_lazy_with(configuration.database.with_db());
+
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,7 +3,6 @@ use once_cell::sync::Lazy;
 use sqlx::{PgConnection, PgPool, Connection, Executor};
 use uuid::Uuid;
 use zero2prod::{configuration::{get_configuration, DatabaseSettings}, telemetry::{get_subscriber, init_subscriber}};
-use secrecy::ExposeSecret;
 
 static TRACING: Lazy<()> = Lazy::new( || {
     let default_filter_level = "info".to_string();
@@ -55,9 +54,7 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(
-            &config.connection_string_without_db().expose_secret()
-        )
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
     connection
@@ -65,7 +62,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
